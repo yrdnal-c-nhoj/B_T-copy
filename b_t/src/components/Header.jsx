b15b8584cdeec31d4c24a1de2c3c19820275cc06
john <0.borrowed.time.0@gmail.com>
2025-05-15 16:34:01 -0400

    x

--- b_t/src/components/Header.jsx
+++ b_t/src/components/Header.jsx
@@ -1,15 +1,12 @@
-import './Header.css'
-
 function Header() {
     return (
         <header>
             <div>
-                <div class="title-container">
-                    <div class="chltitle">Cubist Heart Laboratories</div>
-                    <div class="bttitle">BorrowedTime</div>
+                <div className="title-container">
+                    <div className="chltitle">Cubist Heart Laboratories</div>
+                    <div className="bttitle">BorrowedTime</div>
                 </div>
             </div>
         </header>
     );
 }
-export default Header;
